802a582ab78cdf3228ff027a73f2246defc13d99
vandana147 <31824677+vandana147@users.noreply.github.com>
2017-09-11 13:51:27 +0530

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+	    StudentGroup a=new StudentGroup(5);
+
+		//You may test that your code works find here
+		//Please check that your code works and has no
+		//compilation problems before to submit
+	}
+
+}
